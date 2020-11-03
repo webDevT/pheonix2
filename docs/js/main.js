d87f4562319073a611cb4502826e02e8eda991ad
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-11-03 12:50:26 +0300

    tab jump

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 $(".tab").click(function() {
+	
 	$(".tab").removeClass("active").eq($(this).index()).addClass("active");
  	$(".tab-item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
